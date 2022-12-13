c978fb0d284d9b94dab1bc55ea092023db3edb8b
ventech2 <fumexedu@gmail.com>
2022-12-12 19:43:36 -0800

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - A program that prints a line using the puts function
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 
 }
