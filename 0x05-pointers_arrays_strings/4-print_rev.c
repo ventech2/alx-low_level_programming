88af8926ba026e3063a9daede30fc8fb7b791a0f
ventech2 <fumexedu@gmail.com>
2022-12-19 19:25:03 -0800

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#INCLUDE "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed in reverse
+ *
+ * return: nothing on sucess
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+
+	while (count)
+	{
+		s--;
+		_putcher(*s);
+		count--;
+	}
+
+	_putchar('\n');
+}
